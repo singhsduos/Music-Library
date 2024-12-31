5eb8f173917a5929e560abcb442357017f382537
singhsduos <ns9628491678@gmail.com>
2025-01-01 00:19:01 +0530

    added - add user by admin

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,29 +4,67 @@ const { UserService } = require('../services/user.service');
 const authenticateRequest = require("../middlewares/authenticateRequest");
 
 class UserController {
-  constructor() {
-    this.path = '';
-    this.router = express.Router();
-    this.initRoutes();
-  }
-
-  initRoutes() {
-     this.router.get('/users', authenticateRequest, this.getUsers);
-  }
-
-  async getUsers(req, res, next) {
-    try {
-      const users = await UserService.getUsers(req);
-      res.status(200).json({
-        status: 200,
-        data: users,
-        message: "Users retrieved successfully.",
-        error: null,
-      });
-    } catch (error) {
-      next(error);
+    constructor() {
+        this.path = '/users';
+        this.router = express.Router();
+        this.initRoutes();
     }
-  }
+
+    initRoutes() {
+        this.router.get('/', authenticateRequest, this.getUsers);
+        this.router.post('/add-user', authenticateRequest, this.addUser);
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            const users = await UserService.getUsers(req);
+            res.status(200).json({
+                status: 200,
+                data: users,
+                message: "Users retrieved successfully.",
+                error: null,
+            });
+        } catch (error) {
+            console.log('User List error: ', error.message);
+            if (error instanceof ErrorHandler) {
+                return res.status(error.statusCode).json(
+                    {
+                        "status": error.statusCode,
+                        "data": null,
+                        "message": error.message,
+                        "error": null
+                    }
+                );
+            }
+            next(error);
+        }
+    }
+
+    async addUser(req, res, next) {
+        try {
+            const user = await UserService.addUser(req);
+
+            res.status(201).json({
+                status: 201,
+                data: null,
+                message: 'User created successfully.',
+                error: null
+            });
+        } catch (error) {
+            console.log('Error Creating User: ', error.message);
+            if (error instanceof ErrorHandler) {
+                return res.status(error.statusCode).json(
+                    {
+                        "status": error.statusCode,
+                        "data": null,
+                        "message": error.message,
+                        "error": null
+                    }
+                );
+            }
+            next(error);
+        }
+    }
 
 }
 
--- services/user.service.js
+++ services/user.service.js
@@ -2,15 +2,22 @@ const config = require('config')
 const { User } = require('../models/user.model');
 
 class UserService {
+    checkRequiredFields(fields, requiredFields) {
+        requiredFields.forEach(field => {
+            if (!fields[field]) {
+                throw new ErrorHandler(400, `Bad Request. Missing required field: ${field}`);
+            }
+        });
+    }
     async getUsers(req) {
         try {
-            const {limit, offset, role} = req.query
+            const { limit, offset, role } = req.query
             let matchFilter = {};
 
             if (role) {
                 matchFilter.role = role;
             }
-            
+
             const users = await User.aggregate([
                 { $match: matchFilter },
                 { $skip: parseInt(offset) || 0 },
@@ -25,6 +32,33 @@ class UserService {
         }
     }
 
+    async addUser(req) {
+        try {
+            const { email, password, role } = req.body
+            this.checkRequiredFields({ email, password, role }, ['email', 'password', 'role']);
+
+            if (role === 'Admin') {
+                throw new ErrorHandler(403, 'Forbidden Access: Cannot create an Admin user');
+            }
+
+            const existingUser = await User.findOne({ email });
+            if (existingUser) {
+                throw new ErrorHandler(409, 'Email already exists');
+            }
+
+            const newUser = new User({
+                email,
+                password,
+                role
+            });
+
+            await newUser.save();
+            return newUser;
+        } catch (error) {
+            console.error('Error in addUser service:', error.message);
+            throw new ErrorHandler(error.statusCode || 500, error.message, error);
+        }
+    }
 
 }
 
